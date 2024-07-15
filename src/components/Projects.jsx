ac89345a6cf7c81505043e151d3b17df4ede1a85
PhillipCostas <Phillipcostas@gmail.com>
2024-07-15 10:22:56 -0400

    just a save point

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -16,12 +16,12 @@ const Projects = () => {
       <h2>Travel Packing App</h2>
       <p>
       Creating a packing app for traveling abroad was a labor of love for my wife, aiming to ease her packing process. This project offered an excellent opportunity to work with model schemas, ensuring the app's structure was robust and efficient. Developing a CRUD application allowed for seamless data management, providing flexibility to add, remove, and modify packing items. Crafting intuitive forms was crucial, enhancing user experience and ensuring the app's practicality. This project not only served a personal need but also expanded my expertise in full-stack development.</p>
-      <a href="https://phillipcostas.github.io/Unit-1-project-Game-Simon/" target="_blank" rel="noopener noreferrer">
+      <a href="" target="_blank" rel="noopener noreferrer">
         Travel Packing App
       </a>
       <h2>Application for tracking Job Applications</h2>
       <p>Reasoning why I chose to do this game: ...</p>
-      <a href="https://phillipcostas.github.io/Unit-1-project-Game-Simon/" target="_blank" rel="noopener noreferrer">
+      <a href="https://phillipcostas.github.io/skyrockit" target="_blank" rel="noopener noreferrer">
         Job tracking Application
       </a>
       <h2>Coin Toss - a person to person betting app</h2>
